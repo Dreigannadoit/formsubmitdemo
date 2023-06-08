9b325aaf761f8f9928e7216ffc39c4e1d4f19037
Drei Abmab <70103521+Dreigannadoit@users.noreply.github.com>
2023-06-08 16:01:00 +0800

    Add files via upload

--- send-email.php
+++ send-email.php
@@ -1,3 +1,18 @@
+<?php
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+$mailheader = "From:".$name."<".$email.">\r\n";
+
+$recipient = "dreiabmab@gmail.com";
+
+mail($recipient, $subject, $message, $mailheader)
+or die("Couldn't send");
+
+echo'
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -21,3 +36,6 @@
   </div>
 </body>
 </html>
+'
+
+?>
